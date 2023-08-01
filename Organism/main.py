8b6bbe73112daae25f385c4c80cbd164354f760d
Priyam Srivastava <ipriyam26@gmail.com>
2023-08-01 12:48:01 +0530

    🐛 fix(main.py): fix import error for Gene in main.py ✨ feat(main.py): add genes attribute to Organism class to store a list of Gene objects 🐛 fix(brain.py): fix import error for Gene in brain.py ✨ feat(brain.py): add genome parameter to NeuralNetwork constructor to initialize genes attribute ✨ feat(brain.py): add add_connection method to NeuralNetwork class to add connections to the network 🆕 new(gene.py): add Gene class to handle gene operations 🆕 new(config.py): add INTERNAL_NEURON_COUNT constant 🆕 new(pyproject.toml): add black as a dependency 🗑️ remove(types.py): remove unused types.py file

--- Organism/main.py
+++ Organism/main.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from typing import List
 from organism.neural_network.brain import NeuralNetwork
+from organism.neural_network.gene import Gene
+
 
 @dataclass
 class PhysicalAttributes:
@@ -7,31 +10,37 @@ class PhysicalAttributes:
     y: int
     pheronome: float
     oscillator: float
-    genome:str
-    age:int
-    id:int
+    genome: str
+    age: int
+    id: int
+    genes: List[Gene]
+
 
 class Organism:
-    def __init__(self, x:int, y:int,id:int,genome:str):
-        self.neural_network = NeuralNetwork()
+    def __init__(self, x: int, y: int, id: int, genome: List[str]):
+        self.neural_network = NeuralNetwork(genome=genome)
         self.id = id
-        self.attributes = PhysicalAttributes(x=x,y=y,pheronome=0,oscillator=0,genome=genome,age=0,id=id)
+        self.attributes = PhysicalAttributes(
+            x=x,
+            y=y,
+            pheronome=0,
+            oscillator=0,
+            genome=genome,
+            age=0,
+            id=id,
+            genes=self.neural_network.genes,
+        )
 
     def step(self, environment):
         self.neural_network.process(organism=self, environment=environment)
 
         # Select the ActionNeuron with the highest value
-        max_value = -float('inf')
+        max_value = -float("inf")
         best_action = None
         for action_neuron in self.neural_network.action_neurons:
             if action_neuron.get_value() > max_value:
                 max_value = action_neuron.get_value()
                 best_action = action_neuron.get_action()
-        
+
         if best_action:
             best_action.perform(self)
-
-
-
-
-
--- Organism/neural_network/brain.py
+++ Organism/neural_network/brain.py
@@ -1,32 +1,90 @@
-from typing import List
+from typing import Any, Dict, List
 from organism.main import Organism
 from organism.neural_network.connection import Connection
-from organism.neural_network.neuron import ActionNeuron, InternalNeuron, Neuron, SensoryNeuron
+from organism.neural_network.neuron import (
+    ActionNeuron,
+    InternalNeuron,
+    Neuron,
+    SensoryNeuron,
+)
+from organism.neural_network.gene import Gene
+from config import INTERNAL_NEURON_COUNT
 
 
 class NeuralNetwork:
-    def __init__(self, neurons:List[Neuron], connections:List[Connection], max_iterations=100):
-        for conn in connections:
-            self.neurons[conn.sink.neuron_id].add_connection(conn)
-        self.max_iterations = max_iterations
-        self.sensory_neurons = [neuron for neuron in neurons if isinstance(neuron, SensoryNeuron)]
-        self.internal_neurons = [neuron for neuron in neurons if not isinstance(neuron, InternalNeuron)]
-        self.action_neurons = [neuron for neuron in neurons if isinstance(neuron, ActionNeuron)]
+    def __init__(self, genome: List[str], max_iterations=100):
+        self.__make_neurons()
+        self.genome = genome
+        self.genes = [Gene.from_hex(gene) for gene in genome]
+        connections = [
+            gene.parse(
+                num_sensory_neurons=len(self.sensory_neurons),
+                num_internal_neurons=len(self.internal_neurons),
+                num_action_neurons=len(self.action_neurons),
+            )
+            for gene in self.genes
+        ]
 
-    @classmethod
-    def from_genome(cls, genome:str):
+        self.add_connection(connections)
+
+        self.max_iterations = max_iterations
+
+    def add_connection(self, connections:list[dict[str, Any]]):
+        for conn in connections:
+            source_type: str = conn["source_type"]
+            source_id: int = conn["source_id"]
+            sink_type: str = conn["sink_type"]
+            sink_id: int = conn["sink_id"]
+            weight: float = conn["weight"]
+
+            if source_type == "sensory":
+                source = self.sensory_neurons.get(source_id)
+            elif source_type == "internal":
+                source = self.internal_neurons.get(source_id)
+
+            if sink_type == "internal":
+                sink = self.internal_neurons.get(sink_id)
+            elif sink_type == "action":
+                sink = self.action_neurons.get(sink_id)
+            connection = Connection(source=source, sink=sink, weight=weight)
+            source.add_connection(connection)
+
+    def __make_neurons(self):
+        self.sensory_neurons = self.__make_sensory_neuron()
+        self.internal_neurons = self.__make_internal_neuron()
+        self.action_neurons = self.__make_action_neuron()
+
+    def __make_sensory_neuron(self) -> Dict[int, SensoryNeuron]:
         pass
 
+    def __make_internal_neuron(self) -> Dict[int, InternalNeuron]:
+        start_id = self.sensory_neurons[-1].neuron_id + 1
+        return {
+            i + start_id: InternalNeuron(neuron_id=i + start_id)
+            for i in range(INTERNAL_NEURON_COUNT)
+        }
 
-    def process(self, organism:Organism, environment):
+    def __make_action_neuron(self) -> Dict[int, ActionNeuron]:
+        # start_id = self.internal_neurons[-1].neuron_id + 1
+        pass
 
+    # #TODO: Implement this
+    #     @classmethod
+    #     def from_genome(cls, genome:List[str]):
+    #         genes = [Gene.from_hex(gene) for gene in genome]
+    #         for gene in genes:
+    #             conn = gene.parse()
+
+    def process(self, organism: Organism, environment):
         # Update sensory neurons
         for neuron in self.sensory_neurons:
             neuron.update_value(organism, environment)
-        
+
         # Update internal neurons until values stabilize
         for _ in range(self.max_iterations):
-            value_cache = {neuron.neuron_id: neuron.get_value() for neuron in self.internal_neurons}
+            value_cache = {
+                neuron.neuron_id: neuron.get_value() for neuron in self.internal_neurons
+            }
             change_occurred = False
             for neuron in self.internal_neurons:
                 old_value = value_cache[neuron.neuron_id]
@@ -35,9 +93,10 @@ class NeuralNetwork:
                     change_occurred = True
             if not change_occurred:
                 break
-        
+
         # Update action neurons
-        value_cache = {neuron.neuron_id: neuron.get_value() for neuron in self.internal_neurons}
+        value_cache = {
+            neuron.neuron_id: neuron.get_value() for neuron in self.internal_neurons
+        }
         for neuron in self.action_neurons:
             neuron.update_value(value_cache)
-
